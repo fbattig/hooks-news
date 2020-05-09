d797be7b85710fb84ef9d832fa9fe2cf7fa972eb
Felix Battig <fbattig@kreonovo.ca>
2020-05-09 09:21:56 -0400

    Custom Hooks
    using useFromValidation a Custom Hooks, and adjusted Login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react';
+import useFormValidation from './useFormValidation';
 
+const INITIAL_STATE = {
+	name: '',
+	email: '',
+	password: '',
+};
 function Login(props) {
+  const { handleSubmit, handleChange, values } = useFormValidation(INITIAL_STATE);
 	const [login, setLogin] = useState(true);
+
 	return (
 		<div>
 			<h2 className="mv3">{login ? 'Login' : 'Create Account'}</h2>
-			<form className="flex flex-column">
+			<form className="flex flex-column" onSubmit={handleSubmit}>
 				{!login && (
-					<input type="text" placeholder="Your name" autoComplete="off" />
+					<input
+            type="text"
+            value ={values.name}
+						onChange={handleChange}
+						name="name"
+						placeholder="Your name"
+						autoComplete="off"
+					/>
 				)}
-				<input type="email" placeholder="Your email" autoComplete="off" />
-				<input type="password" placeholder="Choose a secure password" />
+				<input
+          type="email"
+          value ={values.email}
+					onChange={handleChange}
+					name="email"
+					placeholder="Your email"
+					autoComplete="off"
+				/>
+				<input
+          type="password"
+          value ={values.password}
+					onChange={handleChange}
+					name="password"
+					placeholder="Choose a secure password"
+				/>
 				<div className="flex mt3">
 					<button type="submit" className="button pointer mr2">
 						Submit
@@ -20,7 +48,9 @@ function Login(props) {
 						className="pointer button"
 						onClick={() => setLogin((prevLogin) => !prevLogin)}
 					>
-						{login ? 'need to create an account..?' : 'already have an account..?'}
+						{login
+							? 'need to create an account..?'
+							: 'already have an account..?'}
 					</button>
 				</div>
 			</form>
